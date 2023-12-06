3bada17f14e3d63299cbf5a3ff0dc237bcb32bdd
archith1915 <126151104+archith1915@users.noreply.github.com>
2023-12-06 10:09:33 +0530

    Java trial

---
+++ arithmetic.java
@@ -0,0 +1,15 @@
+
+public class arithmetic{
+    public static void main(String args[]){
+        int num1 = 10;
+        int num2 = 15;
+        int sum = num1+num2;
+        int dif = num2-num1;
+        int mul = num1*num2;
+        float div = num1/num2;
+        System.out.println("The summation of given numbers is " + sum );
+        System.out.println("The difference of given numbers is " + dif);
+        System.out.println("The product of given numbers is " + mul);
+        System.out.println("The division of given number is" + div);
+    }
+}
